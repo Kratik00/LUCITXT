336fdab0e114193f94b0b00cb3adb4cd084ccc1c
Kratik00 <159747619+Kratik00@users.noreply.github.com>
2024-12-23 15:44:22 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,3 +1,3 @@
-api_id = 27526569
-api_hash = "1b7c2d98893388c9917cd75855d0a5a6"
-bot_token = "6890697068:AAGKqVkUfV1Vs6GbRCgFdTxv9tr04XBbdD8"
+api_id = 25318125
+api_hash = "b29fb6a928e8b8a3308f8c2d3ba9cfb0"
+bot_token = "7763626443:AAFD8V9n5v6N43rSilPdYGTLEPMtgNtpL3w"
